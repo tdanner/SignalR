d19f3693fa2869bff8c060a889b0a360da40530e
Gustavo Armenta <gustavo_armenta@yahoo.com>
2013-09-05 15:11:41 -0700

    add AsyncManualResetEvent to Functional Tests

--- tests/Microsoft.AspNet.SignalR.FunctionalTests/Server/Connections/ConnectionFacts.cs
+++ tests/Microsoft.AspNet.SignalR.FunctionalTests/Server/Connections/ConnectionFacts.cs
@@ -491,8 +491,8 @@ namespace Microsoft.AspNet.SignalR.Client.Tests
 
                     using (connection)
                     {
-                        var reconnectWh = new ManualResetEventSlim();
-                        var disconnectWh = new ManualResetEventSlim();
+                        var reconnectWh = new AsyncManualResetEvent();
+                        var disconnectWh = new AsyncManualResetEvent();
 
                         connection.Reconnecting += () =>
                         {
@@ -509,8 +509,8 @@ namespace Microsoft.AspNet.SignalR.Client.Tests
                         await connection.Start(host.Transport);
                         host.Shutdown();
 
-                        Assert.True(reconnectWh.Wait(TimeSpan.FromSeconds(25)), "Reconnect never fired");
-                        Assert.True(disconnectWh.Wait(TimeSpan.FromSeconds(25)), "Closed never fired");
+                        Assert.True(await reconnectWh.WaitAsync(TimeSpan.FromSeconds(25)), "Reconnect never fired");
+                        Assert.True(await disconnectWh.WaitAsync(TimeSpan.FromSeconds(25)), "Closed never fired");
                     }
                 }
             }
@@ -542,8 +542,8 @@ namespace Microsoft.AspNet.SignalR.Client.Tests
 
                     using (connection)
                     {
-                        var reconnectingWh = new ManualResetEventSlim();
-                        var reconnectedWh = new ManualResetEventSlim();
+                        var reconnectingWh = new AsyncManualResetEvent();
+                        var reconnectedWh = new AsyncManualResetEvent();
 
                         connection.Reconnecting += () =>
                         {
@@ -562,8 +562,8 @@ namespace Microsoft.AspNet.SignalR.Client.Tests
                         // Force reconnect
                         Thread.Sleep(TimeSpan.FromSeconds(5));
 
-                        Assert.True(reconnectingWh.Wait(TimeSpan.FromSeconds(30)));
-                        Assert.True(reconnectedWh.Wait(TimeSpan.FromSeconds(30)));
+                        Assert.True(await reconnectingWh.WaitAsync(TimeSpan.FromSeconds(30)));
+                        Assert.True(await reconnectedWh.WaitAsync(TimeSpan.FromSeconds(30)));
                         Thread.Sleep(TimeSpan.FromSeconds(15));
                         Assert.NotEqual(ConnectionState.Disconnected, connection.State);
                     }
@@ -584,7 +584,7 @@ namespace Microsoft.AspNet.SignalR.Client.Tests
                 using (var host = CreateHost(hostType, transportType))
                 {
                     var errorsCaught = 0;
-                    var wh = new ManualResetEventSlim();
+                    var wh = new AsyncManualResetEvent();
                     Exception thrown = new Exception(),
                               caught = null;
 
@@ -610,7 +610,7 @@ namespace Microsoft.AspNet.SignalR.Client.Tests
 
                         await connection.Start(host.Transport);
 
-                        Assert.True(wh.Wait(TimeSpan.FromSeconds(5)));
+                        Assert.True(await wh.WaitAsync(TimeSpan.FromSeconds(5)));
                         Assert.Equal(thrown, caught);
                     }
                 }
@@ -641,7 +641,7 @@ namespace Microsoft.AspNet.SignalR.Client.Tests
                     {
                         Debug.WriteLine("Transport: {0}", (object)transport.Name);
 
-                        var wh = new ManualResetEventSlim();
+                        var wh = new AsyncManualResetEvent();
                         Exception thrown = new InvalidOperationException(),
                                   caught = null;
 
@@ -663,7 +663,7 @@ namespace Microsoft.AspNet.SignalR.Client.Tests
                             await connection.Start(transport);
                             var ignore = connection.Send("");
 
-                            Assert.True(wh.Wait(TimeSpan.FromSeconds(5)));
+                            Assert.True(await wh.WaitAsync(TimeSpan.FromSeconds(5)));
                             Assert.Equal(thrown, caught);
                         }
                     }
